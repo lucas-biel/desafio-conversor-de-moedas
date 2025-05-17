99c0d5bf801620a1bf2b671e9843cf42aeddf157
Lucas <santos.lucas1029@gmail.com>
2025-05-17 12:29:20 -0300

    test: testando requisição da api

--- src/Main.java
+++ src/Main.java
@@ -1,2 +1,14 @@
+import java.io.IOException;
+
 public class Main {
+
+    public static void main(String[] args) throws IOException, InterruptedException {
+
+        Request request = new Request();
+
+        ConvertedValue converted = request.convertCurrency("BRL", "USD", "10");
+
+        System.out.println(converted);
+
+    }
 }
